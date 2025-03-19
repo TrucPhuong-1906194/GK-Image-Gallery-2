f26ad59182f486517ae2582f024702f15b8bb33f
TrucPhuong-1906194 <1906194@lsts.edu.vn>
2025-03-19 08:44:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
+//Khai biến
+//Điều khiển bằng chuột hoặc nút bấm (arrow)
 let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
-
+//Lăn chuột
 scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-    scrollContainer.style.scrollBehavior = "auto";
+    evt.preventDefault();                           //sự kiện bị chặn tránh theo chiều dọc
+    scrollContainer.scrollLeft += evt.deltaY;       //gán scrollLeft, cuộn ngang
+    scrollContainer.style.scrollBehavior = "auto";  //auto, không hiệu ứng mượt
 });
-
+//Lăn chuột bằng nút tiếp theo
 nextBtn.addEventListener("click", ()=>{
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 900;
+    scrollContainer.style.scrollBehavior = "smooth";//cuộn mượt
+    scrollContainer.scrollLeft += 900;              //cuộn sang phải 900px
 });
 
-backBtn.addEventListener("click", ()=>{
+backBtn.addEventListener("click", ()=>{             //ngược lại
     scrollContainer.style.scrollBehavior = "smooth";
     scrollContainer.scrollLeft -= 900;
 });
 
 // Mở rộng
 
-var icon = document.getElementById("icon");
-icon.src = "images/icon-theme.jpg"
-icon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "images/icon-flipped.png";
+var icon = document.getElementById("icon");         //khai biến
+icon.src = "images/icon-theme.jpg"                  //gán ảnh mặc định
+icon.onclick = function(){                          //chuyển đổi theme khi bấm icon
+    document.body.classList.toggle("dark-theme");   //toggle như công thức, class được bật/tắt (ban đầu); có/chưa thì xóa/thêm class
+    if(document.body.classList.contains("dark-theme")){ 
+        icon.src = "images/icon-flipped.png";           
     } 
     else {
         icon.src = "images/icon-theme.jpg";
